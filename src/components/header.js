2b17c4bf7b000308cd0ac4559aea73851a2bee91
Mike <michaelmifsudsweeney@gmail.com>
2022-06-17 14:31:18 -0400

    Added header state changes

--- src/components/header.js
+++ src/components/header.js
@@ -4,24 +4,47 @@ import { Link } from "gatsby"
 import logo from '../images/logo-designcode.svg'
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="header">
-    <div className="headerGroup">
-      <Link to="/"> <img src={require('../images/logo-designcode.svg').default} width="30"/></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>Buy</button></Link>
-    </div>
-  </div>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+class Header extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      hasScrolled:false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll',
+    this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if(scrollTop > 50) {
+      this.setState({hasScrolled : true})
+    } else {
+      this.setState({hasScrolled : false})
+    }
+  }
+  
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'header headerScrolled' : 'header'}>
+        <div className="headerGroup">
+          <Link to="/"> <img src={require('../images/logo-designcode.svg').default} width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
+      </div>
+
+    )
+
+  }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
